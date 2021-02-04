47322b7cfcab2cb16dcc17517881f55eccc0066e
Павел <yaroshenkopavel82@gmail.com>
2021-02-04 04:40:28 +0100

    3.3. Разбор первой программы. [# 463134]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println( "Ярошенко Павел" );
+        System.out.println( " 22.02.1982 " );
+    }
+}
